fe8df5e73eb06b3c03b8899d44a12b5ffaa54c34
Laura Jarvis <laurjarv654@ed.amdsb.ca>
2020-10-20 14:04:02 -0400

    Day 1 of Brick Breaker design.

---
+++ BrickBreaker/Screens/ControlsScreen.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace BrickBreaker.Screens
+{
+    partial class ControlsScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.returnButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::BrickBreaker.Properties.Resources.Controls_Title;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(543, 145);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // returnButton
+            // 
+            this.returnButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(160)))), ((int)(((byte)(84)))));
+            this.returnButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(63)))), ((int)(((byte)(33)))));
+            this.returnButton.FlatAppearance.BorderSize = 5;
+            this.returnButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.returnButton.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.returnButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(63)))), ((int)(((byte)(33)))));
+            this.returnButton.Location = new System.Drawing.Point(799, 553);
+            this.returnButton.Margin = new System.Windows.Forms.Padding(4);
+            this.returnButton.Name = "returnButton";
+            this.returnButton.Size = new System.Drawing.Size(242, 96);
+            this.returnButton.TabIndex = 2;
+            this.returnButton.Text = "RETURN";
+            this.returnButton.UseVisualStyleBackColor = false;
+            // 
+            // ControlsScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.returnButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ControlsScreen";
+            this.Size = new System.Drawing.Size(1067, 677);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button returnButton;
+    }
+}

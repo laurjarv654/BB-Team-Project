fd29f14dbfa88dbd71484cc491fe06b81f64de18
Sammy McNab <samumcna930@ed.amdsb.ca>
2020-10-22 12:56:19 -0400

    asd

--- BrickBreaker/Screens/GameScreen.cs
+++ BrickBreaker/Screens/GameScreen.cs
@@ -374,10 +374,6 @@ namespace BrickBreaker
                     ball.ySpeed = 6;
                 }
 
-
-                
-               
-                
                 // Moves the ball back to origin
                 ball.x = ((paddle.x - (ball.size / 2)) + (paddle.width / 2));
                 ball.y = (this.Height - paddle.height) - 85;

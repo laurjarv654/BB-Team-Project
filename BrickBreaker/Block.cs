f15dad2a1ef810ea2750a94ccc811c4fb4ceb9be
Sammy McNab <samumcna930@ed.amdsb.ca>
2020-10-22 12:43:39 -0400

    Added highScore class

--- BrickBreaker/Block.cs
+++ BrickBreaker/Block.cs
@@ -15,7 +15,6 @@ namespace BrickBreaker
         public int x;
         public int y; 
         public int hp;
-        public Color colour;
         public Image image;
         public static Random rand = new Random();
 

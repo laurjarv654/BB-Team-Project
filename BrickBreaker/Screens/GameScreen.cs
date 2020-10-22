a9edaae50c72c8a93cd3ff65d39a43e9c79d67e0
Sammy McNab <samumcna930@ed.amdsb.ca>
2020-10-22 09:32:28 -0400

    Changed blocks to images.

--- BrickBreaker/Screens/GameScreen.cs
+++ BrickBreaker/Screens/GameScreen.cs
@@ -210,7 +210,7 @@ namespace BrickBreaker
             blocks.Clear();
             int x = 10;
 
-            while (blocks.Count < 12)
+            while (blocks.Count < 15)
             {
                 x += 57;
                 Block b1 = new Block(x, 10, 1, Color.White);
@@ -392,7 +392,7 @@ namespace BrickBreaker
             // Draws blocks
             foreach (Block b in blocks)
             {
-                e.Graphics.FillRectangle(blockBrush, b.x, b.y, b.width, b.height);
+                e.Graphics.DrawImage(Properties.Resources.greenBrick, b.x, b.y, b.width, b.height);
             }
 
             if (sheildSpawn == true)

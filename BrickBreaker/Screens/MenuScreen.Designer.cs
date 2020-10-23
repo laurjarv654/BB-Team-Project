f5e9d5602d61359b9a76b33630127d44f05fe29c
Sammy McNab <samumcna930@ed.amdsb.ca>
2020-10-23 09:37:13 -0400

    asdf

--- BrickBreaker/Screens/MenuScreen.Designer.cs
+++ BrickBreaker/Screens/MenuScreen.Designer.cs
@@ -57,7 +57,7 @@
             this.playButton.Margin = new System.Windows.Forms.Padding(4);
             this.playButton.Name = "playButton";
             this.playButton.Size = new System.Drawing.Size(164, 76);
-            this.playButton.TabIndex = 0;
+            this.playButton.TabIndex = 1;
             this.playButton.Text = "PLAY";
             this.playButton.UseVisualStyleBackColor = false;
             this.playButton.Click += new System.EventHandler(this.playButton_Click);
@@ -78,7 +78,7 @@
             this.exitButton.Margin = new System.Windows.Forms.Padding(4);
             this.exitButton.Name = "exitButton";
             this.exitButton.Size = new System.Drawing.Size(164, 76);
-            this.exitButton.TabIndex = 1;
+            this.exitButton.TabIndex = 4;
             this.exitButton.Text = "EXIT\r\n";
             this.exitButton.UseVisualStyleBackColor = false;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
@@ -121,7 +121,7 @@
             this.highscoreButton.Name = "highscoreButton";
             this.highscoreButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.highscoreButton.Size = new System.Drawing.Size(164, 76);
-            this.highscoreButton.TabIndex = 6;
+            this.highscoreButton.TabIndex = 3;
             this.highscoreButton.Text = "HIGH SCORE";
             this.highscoreButton.UseVisualStyleBackColor = false;
             this.highscoreButton.Click += new System.EventHandler(this.highscoreButton_Click);
